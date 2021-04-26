ae630b018f39911a48b57f8792c6261b11bd1817
Mknauss <mknauss97@gmail.com>
2021-04-25 22:38:54 -0400

    added questions to the index

--- Develop/index.js
+++ Develop/index.js
@@ -1,18 +1,71 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs")
+const inquirer = require("inquirer")
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: "input",
-        
-    }
-];
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of the project?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a short description of your project. "
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Describe the steps to install if needed. ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What is this project used for?"
+        },
+        {
+            type: "checkbox",
+            name: "license",
+            message: "Which license(s) is your project under? ",
+            choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
+        {
+            type: "input",
+            name: "contribution",
+            message: "Who are the contributors of this projects?"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Is there a test included and if so how did you make sure it works correctly?"
+        },
+        {
+            type: "input",
+            name: "Github",
+            message: "Please enter your GitHub username: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email: "
+        }
+    ]);
+} 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
